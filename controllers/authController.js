533ae96599d00ca29da616487bc1fd4c03ef4fc6
Dimdim28 <wwwdimanes1@gmail.com>
2023-01-18 14:10:10 +0200

    Added logic to handleAuth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,28 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-const handleAuth = (req, res) => {
-
+const handleAuth = async (req, res) => {
+  try {
+    if (!req.body.email)
+      return res.status(400).json({ message: "Email required" });
+    if (!req.body.password)
+      return res.status(400).json({ message: "Password required" });
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(404).json({ message: "Invalid login or password" });
+    const isValidPass = await bcrypt.compare(req.body.password, user.password);
+    if (!isValidPass)
+      return res.status(404).json({ message: "Invalid email or password" });
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Could not log in",
+    });
+  }
   res.end();
 };
 
 module.exports = {
-  handleAuth
+  handleAuth,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { handleAuth } = require('../controllers/AuthController.js');
+const { handleAuth } = require("../controllers/authController.js");
 
-router.post('/', handleAuth);
+router.post("/", handleAuth);
 
 module.exports = router;
