661fcbf4d5f2b35ab2b44b4d1c34df9273b6ff80
den <denissytnikmacho@gmail.com>
2023-01-19 15:20:07 +0200

    made delete controller and some small corrections

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,33 @@ const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
-const registerUser = async (req, res) => {
+const getOneUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) return res.status(404).json({ message: 'Could not find' });
+
+    const { password, ...userData } = user._doc;
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Could not find' });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users)
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Could not find' });
+  }
+}
+
+const createUser = async (req, res) => {
   try {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: 'Incorrect data', errors: errors.array() });
-    }
+    if (!errors.isEmpty()) return res.status(400).json({ message: 'Incorrect data', errors: errors.array() });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -23,35 +44,21 @@ const registerUser = async (req, res) => {
 
     res.json(user);
   } catch (err) {
-    res.status(500).json({
-      message: "Could not register"
-    });
-  }
-};
-
-const getOneUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ _id: req.params.id });
-    if (!user) res.status(404).json({ message: 'Could not find' });
-
-    const { password, ...userData } = user._doc;
-    res.json(userData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+    res.status(500).json({ message: 'Could not register' });
   }
 };
 
 const updateUser = async (req, res) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ message: 'Incorrect data', errors: errors.array() });
 
     let hashedPassword = undefined;
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       hashedPassword = await bcrypt.hash(req.body.password, salt); 
     }
+
     await User.updateOne(
       {
         _id: req.params.id
@@ -67,17 +74,27 @@ const updateUser = async (req, res) => {
     res.json({ message: 'Success' });
   } catch (err) {
     console.log(err);
-    res.status(500).json({
-      message: "Could not update",
-    });
+    res.status(500).json({ message: 'Could not update' });
   }
 };
 
-const deleteUser = (req, res) => {};
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+
+    if (!user) return res.status(404).json({ message: 'Could not find' });
+
+    res.json({ message: 'Success' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Could not delete' });
+  }
+};
 
 module.exports = {
-  registerUser,
   getOneUser,
+  getAllUsers,
+  createUser,
   updateUser,
   deleteUser
 };
