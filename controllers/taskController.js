5d42f19d2616ed5aae1b59c76dbd5c33a3e4307c
den <denissytnikmacho@gmail.com>
2023-05-28 18:37:45 +0300

    feature: created controller for task statistics

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -242,6 +242,60 @@ const shareTask = async (req, res) => {
   }
 };
 
+const getTaskStats = async (req, res) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month =
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  const day = date.getDate();
+  const tomorrowMidnight = new Date(`${year}-${month}-${day + 1}`);
+
+  try {
+    const today = new Date(`${year}-${month}-${day}`);
+    const foundTasks = await Task.find({
+      user: req.userId,
+      dateOfCompletion: {
+        $lte: tomorrowMidnight,
+        $gte: new Date(today.setDate(today.getDate() - 10)),
+      },
+    });
+
+    const stats = [];
+
+    for (let i = 0; i < 10; i++) {
+      const now = new Date(`${year}-${month}-${day}`);
+      const now2 = new Date(`${year}-${month}-${day}`);
+      const now3 = new Date(`${year}-${month}-${day}`);
+
+      const dayStats = {
+        date: new Date(now.setDate(now.getDate() - i)),
+        counter: 0,
+      };
+
+      const dayBefore = new Date(now2.setDate(now2.getDate() - i));
+      const dayAfter = new Date(now3.setDate(now3.getDate() - i + 1));
+
+      foundTasks.forEach((task) => {
+        if (
+          task.dateOfCompletion >= dayBefore &&
+          task.dateOfCompletion < dayAfter
+        ) {
+          dayStats.counter++;
+        }
+      });
+
+      stats.push(dayStats);
+    }
+
+    res.json(stats);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
 module.exports = {
   createTask,
   getAllTasks,
@@ -249,4 +303,5 @@ module.exports = {
   deleteTask,
   updateTask,
   shareTask,
+  getTaskStats
 };
