ad3790b2cd152bb477d5498d6eda255dba20a4ef
den <denissytnikmacho@gmail.com>
2023-06-06 22:44:10 +0300

    fix: replaced data base uri with environment variable

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ const handleAuth = async (req, res) => {
       {
         _id: user._id,
       },
-      "secret123",
+      process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "30d" }
     );
 
