0fe724a48ff17c2e8e447fb9ce728986f7d86b60
LikerFeed <vanya18845@gmail.com>
2023-06-07 19:56:35 +0300

    test: added first passwordController test

---
+++ controllers/passwordController.test.js
@@ -0,0 +1,61 @@
+const { verifyPass } = require('./passwordController');
+const User = require('../models/User');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+
+// Mock the required modules
+jest.mock('../models/User');
+jest.mock('express-validator');
+jest.mock('bcrypt');
+
+describe('passwordController', () => {
+  // Mock request and response objects
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = {
+      userId: 'user123',
+      body: {
+        password: 'password123',
+      },
+    };
+
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('verifyPass', () => {
+    test('should return "Success" if the password is valid', async () => {
+      const validationResultMock = {
+        isEmpty: jest.fn().mockReturnValue(true),
+        array: jest.fn().mockReturnValue([]),
+      };
+
+      const userMock = {
+        _id: 'user123',
+        password: await bcrypt.hash('password123', 10), // Hash the password for comparison
+      };
+
+      validationResult.mockReturnValue(validationResultMock);
+      User.findOne.mockResolvedValue(userMock);
+      bcrypt.compare.mockResolvedValue(true);
+
+      await verifyPass(req, res);
+
+      expect(validationResult).toHaveBeenCalled();
+      expect(User.findOne).toHaveBeenCalledWith({ _id: req.userId });
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        req.body.password,
+        userMock.password
+      );
+      expect(res.json).toHaveBeenCalledWith({ message: 'Success' });
+    });
+  });
+});
