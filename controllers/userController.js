f7ce45a3019bbc23712ceaa57f32a8241f12e0dc
den <denissytnikmacho@gmail.com>
2023-06-07 20:42:25 +0300

    fix: fixed a bug when user password was null

--- controllers/userController.js
+++ controllers/userController.js
@@ -71,24 +71,36 @@ const updateUser = async (req, res) => {
         .status(400)
         .json({ message: "Incorrect data", errors: errors.array() });
 
+    const { username = null, password = null, email = null } = req.body;
+
     let hashedPassword = null;
-    if (req.body.password) {
+    if (password) {
       const salt = await bcrypt.genSalt(10);
-      hashedPassword = await bcrypt.hash(req.body.password, salt);
+      hashedPassword = await bcrypt.hash(password, salt);
+    }
+
+    const newUserData = {};
+    if (hashedPassword) {
+      newUserData.password = hashedPassword;
+    }
+
+    if (username) {
+      newUserData.username = username;
+    }
+
+    if (email) {
+      newUserData.email = email;
     }
 
     const user = await User.findOneAndUpdate(
       {
         _id: req.params.id,
       },
-      {
-        username: req.body.username,
-        password: hashedPassword,
-        email: req.body.email,
-      }
+      newUserData
     );
 
-    if (!user) return res.status(404).json({ message: "Could not find the user" });
+    if (!user)
+      return res.status(404).json({ message: "Could not find the user" });
 
     res.json({ message: "Success" });
   } catch (err) {
