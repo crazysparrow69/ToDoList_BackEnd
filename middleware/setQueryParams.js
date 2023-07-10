1f87fba89104f7e2f190e2eec8b91eb82c7cb1ae
den <denissytnikmacho@gmail.com>
2023-07-10 16:44:32 +0300

    feature: created middleware for setting task query params

---
+++ middleware/setQueryParams.js
@@ -0,0 +1,78 @@
+const setTaskQueryParams = (req, res, next) => {
+  const {
+    page = 1,
+    limit = 10,
+    deadline = "all",
+    categories = [],
+    ...params
+  } = req.query;
+
+  try {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month =
+      date.getMonth() + 1 < 10
+        ? `0${date.getMonth() + 1}`
+        : date.getMonth() + 1;
+    const day = date.getDate();
+    const todayMidnight = new Date(`${year}-${month}-${day}`);
+
+    const queryParams = {
+      user: req.user._id.toString(),
+      ...params,
+    };
+
+    if (categories.length === 1) {
+      queryParams.categories = {
+        $elemMatch: {
+          _id: categories[0]._id,
+        },
+      };
+    } else if (categories.length > 1) {
+      const queryArr = [];
+      categories.forEach((elem) =>
+        queryArr.push({ categories: { $elemMatch: { _id: elem._id } } })
+      );
+      queryParams.$and = queryArr;
+    }
+
+    if (deadline === "day") {
+      queryParams.deadline = todayMidnight;
+    } else if (deadline == "week") {
+      const today = new Date(`${year}-${month}-${day}`);
+      queryParams.deadline = {
+        $gte: todayMidnight,
+        $lte: new Date(today.setDate(today.getDate() + 7)),
+      };
+    } else if (deadline === "month") {
+      const today = new Date(`${year}-${month}-${day}`);
+      queryParams.deadline = {
+        $gte: todayMidnight,
+        $lte: new Date(today.setMonth(today.getMonth() + 1)),
+      };
+    } else if (deadline === "year") {
+      queryParams.deadline = {
+        $gte: todayMidnight,
+        $lte: new Date(`${year + 1}-${month}-${day}`),
+      };
+    } else if (deadline === "outdated") {
+      queryParams.deadline = {
+        $lt: todayMidnight,
+      };
+    } else if (deadline === "nodeadline") {
+      queryParams.deadline = null;
+    } else if (deadline !== "all") {
+      return res.status(404).json({ message: "Tasks page not found" });
+    }
+
+    req.queryParams = queryParams;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = {
+  setTaskQueryParams,
+};
