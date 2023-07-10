8ab07b051e1116f22c6390989effd42353a4a11f
den <denissytnikmacho@gmail.com>
2023-07-10 15:26:41 +0300

    feature: added method for getting user statistics

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const Task = require("./Task");
+
 const userSchema = new Schema(
   {
     username: {
@@ -21,5 +23,54 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.getStats = async function () {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month =
+    date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  const day = date.getDate();
+  const tomorrowMidnight = new Date(`${year}-${month}-${day + 1}`);
+
+  const today = new Date(`${year}-${month}-${day}`);
+  const foundTasks = await Task.find({
+    user: this._id.toString(),
+    dateOfCompletion: {
+      $lte: tomorrowMidnight,
+      $gte: new Date(today.setDate(today.getDate() - 10)),
+    },
+  });
+
+  const stats = [];
+
+  for (let i = 0; i < 10; i++) {
+    const now = new Date(`${year}-${month}-${day}`);
+    const now2 = new Date(`${year}-${month}-${day}`);
+    const now3 = new Date(`${year}-${month}-${day}`);
+
+    const dayStats = {
+      date: new Date(now.setDate(now.getDate() - i)),
+      counter: 0,
+    };
+
+    const dayBefore = new Date(now2.setDate(now2.getDate() - i));
+    const dayAfter = new Date(now3.setDate(now3.getDate() - i + 1));
+
+    foundTasks.forEach((task) => {
+      if (
+        task.dateOfCompletion >= dayBefore &&
+        task.dateOfCompletion < dayAfter
+      ) {
+        dayStats.counter++;
+      }
+    });
+
+    stats.push(dayStats);
+  }
+
+  stats.reverse();
+
+  return stats;
+};
 
 module.exports = mongoose.model("user", userSchema);
